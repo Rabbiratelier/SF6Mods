6cd75c394583b649881e2914f14d4cfa1f2aa287
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-08-11 06:10:17 +0900

    Switches hook from name getter to object creation
    Replaces the hook on retrieving object names with a hook on object creation
    to improve tracking of unique game objects. Commenting out the old logic
    avoids redundant name checks and may enhance performance or reliability.

--- experimental/reframework/autorun/_exclusive_find_game_object.lua
+++ experimental/reframework/autorun/_exclusive_find_game_object.lua
@@ -4,9 +4,20 @@ local setup_hook = require("func/setup_hook")
 
 local names = {}
 
-setup_hook("via.GameObject", "get_Name", function(args)
-    local obj = sdk.to_managed_object(args[2])
-    local name = obj:get_Name()
+-- setup_hook("via.GameObject", "get_Name", function(args)
+--     local obj = sdk.to_managed_object(args[2])
+--     local name = obj:get_Name()
+--     for _, v in pairs(names) do
+--         if v == name then
+--             return sdk.PreHookResult.CALL_ORIGINAL
+--         end
+--     end
+--     table.insert(names, name)
+-- end)
+
+setup_hook("via.GameObject", "create", function(args)
+    -- local obj = sdk.to_managed_object(args[2])
+    local name = sdk.to_managed_object(args[3]):ToString()
     for _, v in pairs(names) do
         if v == name then
             return sdk.PreHookResult.CALL_ORIGINAL
