d5d3241cb6da98094a76ba9785b7998a7bfdf607
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-08-14 05:58:50 +0900

    Fix: Update method to retrieve SecondaryListIndex using colon syntax

--- experimental/reframework/autorun/_exclusive_exittodesktop.lua
+++ experimental/reframework/autorun/_exclusive_exittodesktop.lua
@@ -88,7 +88,7 @@ local names = {}
 
 setup_hook("app.training.UIFlowTrainingMenu.Param", "InitSecondaryList", function(args)
     local param = sdk.get_managed_singleton("app.training.TrainingManager")._UITrainingMenu._ParamData
-    local index = param.get_SecondaryListIndex()
+local index = param:get_SecondaryListIndex()
     table.insert(names, "SecondaryListIndex: " .. index)
 end)
 
