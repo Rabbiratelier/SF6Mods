da0c04c8510e88dd74133943964b619c210c317c
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-08-31 15:04:37 +0900

    Fix message confirmation to use correct method and update message box parameters

--- misc/reframework/autorun/_exclusive_exittodesktop.lua
+++ misc/reframework/autorun/_exclusive_exittodesktop.lua
@@ -45,9 +45,12 @@ function this.set_is_in_training(value)
     end
 end
 function this.create_message_confirmation()
-    return sdk.find_type_definition("app.helper.hMsg"):get_method("GetMessageId(System.String, System.UInt32)"):call(nil, "CommonMessage", 782):ToString()
+    return sdk.find_type_definition("app.helper.hMsg"):get_method("GetMessage(System.String, System.UInt32)"):call(nil, "CommonMessage", 782)
 end
 
+
+this.confirmation_title = this.create_message_confirmation()
+re.msg(this.confirmation_title)
 
 
 -- Manage Training State Using Hooks
@@ -78,7 +81,7 @@ setup_hook("app.UIPartsGroupItem", "get_CanDecide()", function(args)
 end, function(retval)
     local str = thread.get_hook_storage()["this"]
     if str then
-        this._msg_handle = sdk.find_type_definition("app.UIFlowDialog.MessageBox"):get_method("Start"):call("a" .. this.create_message_confirmation(), "Are you sure want to return to " .. str .. "?", nil, 0, 1, 4, -1, 1)
+        this._msg_handle = sdk.find_type_definition("app.UIFlowDialog.MessageBox"):get_method("Start"):call(this.confirmation_title, "Are you sure want to return to " .. str .. "?", nil, 0, 1, 4, -1, 1)
         this._training_manager:Save(nil, nil)
     end
     return retval
