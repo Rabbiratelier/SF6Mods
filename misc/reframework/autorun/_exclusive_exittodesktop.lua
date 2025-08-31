5d97e758805196338d97ab75f5bee3444429e36c
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-08-31 21:29:11 +0900

    Fix child data assignment to use pairs for iteration in training state management

--- misc/reframework/autorun/_exclusive_exittodesktop.lua
+++ misc/reframework/autorun/_exclusive_exittodesktop.lua
@@ -41,7 +41,7 @@ function this.set_is_in_training(value)
             _target._ChildData = sdk.create_managed_array("app.training.TrainingMenuData", 2)
             _target._ChildData[0] = sdk.find_type_definition("app.training.TrainingMenuData"):create_instance()
             _target._ChildData[1] = sdk.find_type_definition("app.training.TrainingMenuData"):create_instance()
-            for _, child in ipairs(_target._ChildData) do
+            for _, child in pairs(_target._ChildData) do
                 child:call(".ctor")
                 child._Type = 20
                 child._FuncType = 0
