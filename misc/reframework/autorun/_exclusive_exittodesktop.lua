97ad91761cbae1570253387369c8d1fcff716b13
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-08-31 13:37:48 +0900

    Fix message box call parameters for exit confirmation in training mode

--- misc/reframework/autorun/_exclusive_exittodesktop.lua
+++ misc/reframework/autorun/_exclusive_exittodesktop.lua
@@ -75,7 +75,7 @@ setup_hook("app.UIPartsGroupItem", "get_CanDecide()", function(args)
 end, function(retval)
     local str = thread.get_hook_storage()["this"]
     if str then
-        this._msg_handle = sdk.find_type_definition("app.UIFlowDialog.MessageBox"):get_method("Start"):call(nil, "Are you sure want to return to " .. str .. "?", nil, 0, 1, 4, -1, 1)
+        this._msg_handle = sdk.find_type_definition("app.UIFlowDialog.MessageBox"):get_method("Start"):call(nil, "Are you sure want to return to " .. str .. "?", nil, 0, 1, 4, 0, 1)
         this._training_manager:Save(nil, nil)
     end
     return retval
