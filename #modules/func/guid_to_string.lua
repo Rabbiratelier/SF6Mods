e1bb0517d2d4bb8110c5d9c03bb48fc6e58a8203
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-07 22:28:59 +0900

    Remove guid_to_string function to streamline GUID handling across modules

--- #modules/func/guid_to_string.lua
+++
@@ -1,8 +0,0 @@
-function guid_to_string(guid)
-    return string.format("%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X",
-        guid.mData1, guid.mData2, guid.mData3,
-        guid.mData4_0, guid.mData4_1,
-        guid.mData4_2, guid.mData4_3, guid.mData4_4, guid.mData4_5, guid.mData4_6, guid.mData4_7)
-end
-
-return guid_to_string
