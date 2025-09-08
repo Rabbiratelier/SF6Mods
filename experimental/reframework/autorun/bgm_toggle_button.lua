e6a392796029e63f031ffa736d94be5bb721777b
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-08 21:29:20 +0900

    Simplify button press handling for BGM toggle button

--- experimental/reframework/autorun/bgm_toggle_button.lua
+++ experimental/reframework/autorun/bgm_toggle_button.lua
@@ -9,7 +9,6 @@ my.mod = {
     NAME = "bgm_toggle_button",
 }
 my.mod.active = true
-my.was_button_down = false
 
 my.TYPE_BGM_VOLUME = load_enum("app.Option.ValueType").SoundBGMVolume
 my.current_bgm_volume = sdk.find_type_definition("app.Option"):get_method("GetOptionValue(app.Option.ValueType)"):call(nil, my.TYPE_BGM_VOLUME)
@@ -17,11 +16,9 @@ my.set_volume_method = sdk.find_type_definition("app.Option"):get_method("Update
 
 re.on_frame(function()
     imgui.begin_window(my.mod.NAME, nil, 0x101E1)
-    local is_down = imgui.button("BUTTON")
-    imgui.end_window()
-    if not my.was_button_down and is_down then
+    if imgui.button("BUTTON") then
         my.current_bgm_volume = my.current_bgm_volume > 0 and 0 or 10
         my.set_volume_method:call(nil, my.TYPE_BGM_VOLUME, my.current_bgm_volume, false)
     end
-    my.was_button_down = is_down
+    imgui.end_window()
 end)
