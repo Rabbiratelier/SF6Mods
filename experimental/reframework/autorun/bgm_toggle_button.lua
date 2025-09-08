91215e742a0a4293c5a9a62cb01cfcc03c3bdc6a
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-08 21:01:02 +0900

    Refactor BGM volume toggle logic for improved readability and efficiency

--- experimental/reframework/autorun/bgm_toggle_button.lua
+++ experimental/reframework/autorun/bgm_toggle_button.lua
@@ -11,12 +11,14 @@ my.mod.active = true
 my.was_button_down = false
 
 my.TYPE_BGM_VOLUME = load_enum("app.Option.ValueType").SoundBGMVolume
+my.current_bgm_volume = sdk.find_type_definition("app.Option"):get_method("GetOptionValue(app.Option.ValueType)"):call(nil, my.TYPE_BGM_VOLUME)
+my.set_volume = sdk.find_type_definition("app.Option"):get_method("SetOptionValue(app.Option.ValueType, System.Int32, System.Boolean)"):call
 
 re.on_frame(function()
     local is_down = imgui.button("Butt")
     if not my.was_button_down and is_down then
-        local bgm_volume = sdk.find_type_definition("app.Option"):get_method("GetOptionValue(app.Option.ValueType)"):call(nil, my.TYPE_BGM_VOLUME) > 0 and 0 or 10
-        sdk.find_type_definition("app.Option"):get_method("UpdatedOptionValueEvent(app.Option.ValueType, System.Int32, System.Boolean)"):call(nil, my.TYPE_BGM_VOLUME, bgm_volume, false)
+        my.current_bgm_volume = my.current_bgm_volume > 0 and 0 or 10
+        my.set_volume(nil, my.TYPE_BGM_VOLUME, my.current_bgm_volume, false)
     end
     my.was_button_down = is_down
 end)
