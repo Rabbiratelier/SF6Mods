c406470ef5156ac9a05d001ce2be55558b3b43fb
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-09 20:16:30 +0900

    Fix child units initialization in option manager by retrieving from the instance instead of creating an empty table

--- experimental/reframework/autorun/optionmanip.lua
+++ experimental/reframework/autorun/optionmanip.lua
@@ -22,7 +22,7 @@ function my.init()
     local _man = sdk.get_managed_singleton("app.OptionManager")
     local _item = sdk.create_instance("app.Option.OptionGroupUnit")
     local _setting = sdk.create_instance("app.Option.OptionSettingUnit")
-    local _child_units = {}
+    local _child_units = _item:get_field("<ChildUnits>k__BackingField")
 
     -- _setting.TypeId = 101
     _setting.TitleMessage = create_message_guid("Mod Options")
@@ -31,8 +31,7 @@ function my.init()
     _item:Setup(_setting)
     _setting.DescriptionMessage = create_message_guid("Options for various mods.")
 
-    re.msg(_item:get_field("<ChildUnits>k__BackingField"):get_type_definition():get_name())
-    table.insert(_child_units, my.init_child())
+    _child_units:Add(my.init_child())
     _item:set_ChildUnits(_child_units)
     
     my._parent_list = _man.UnitLists:get_Item(load_enum("app.Option.TabType").General)
