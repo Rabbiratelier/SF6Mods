bc47e497fbc5a7e0924786503f276a4ddc0225e4
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-13 00:55:26 +0900

    Add exclusive_vsync_maintainer.lua to manage VSync display functionality

---
+++ misc/reframework/autorun/exclusive_vsync_maintainer.lua
@@ -0,0 +1,17 @@
+local re = re
+local sdk = sdk
+local draw = draw
+
+local show_custom_ticker = require("func/show_custom_ticker")
+local was_key_down = require("func/was_key_down")
+
+
+local x_start = 0
+local y_start = 0
+re.on_application_entry("UpdateHID", function())
+    if was_key_down(0x7B) then -- F12
+        draw.filled_rect(x_start, y_start, 200, 20, 0xC0000000)
+        draw.outline_rect(x_start, y_start, 200, 20, 0xFFFFFFFF)
+        -- show_custom_ticker("VSync is")
+    end
+end
