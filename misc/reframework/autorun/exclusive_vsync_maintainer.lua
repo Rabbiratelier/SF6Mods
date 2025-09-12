0ee8ce91f901ed01e3434a8ea2377dbbd515e7e7
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-13 02:46:09 +0900

    Set default show_window state to true in VSync maintainer

--- misc/reframework/autorun/exclusive_vsync_maintainer.lua
+++ misc/reframework/autorun/exclusive_vsync_maintainer.lua
@@ -18,7 +18,7 @@ local height_max = 81
 local font = imgui.load_font(nil, 24)
 local vsync_status_str = sdk.find_type_definition("app.Option"):get_method("GetOptionValue"):call(nil, load_enum("app.Option.ValueType").Vsync) == 0 and "ON" or "OFF"
 local save = json.load_file("vsync_maintainer_save.json") or {}
-local show_window = save[1] or false
+local show_window = save[1] or true
 local que_toggle = false
 
 local function toggle_vsync()
