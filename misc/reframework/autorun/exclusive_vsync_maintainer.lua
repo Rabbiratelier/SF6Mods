79c0ef950cf7281a6064fdc261d98f73efa1f3a5
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-13 00:57:08 +0900

    Refactor exclusive_vsync_maintainer.lua to use on_frame for VSync display functionality

--- misc/reframework/autorun/exclusive_vsync_maintainer.lua
+++ misc/reframework/autorun/exclusive_vsync_maintainer.lua
@@ -8,10 +8,10 @@ local was_key_down = require("func/was_key_down")
 
 local x_start = 0
 local y_start = 0
-re.on_application_entry("UpdateHID", function())
-    if was_key_down(0x7B) then -- F12
+re.on_frame(function()
+    if true then -- was_key_down(0x7B) then -- F12
         draw.filled_rect(x_start, y_start, 200, 20, 0xC0000000)
         draw.outline_rect(x_start, y_start, 200, 20, 0xFFFFFFFF)
         -- show_custom_ticker("VSync is")
     end
-end
+end)
