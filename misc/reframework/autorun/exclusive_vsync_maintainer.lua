f91efa3952cc2b4971cbfc628b3abb92624e0285
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-13 01:14:51 +0900

    Update rectangle position and dimensions in exclusive_vsync_maintainer.lua for VSync display

--- misc/reframework/autorun/exclusive_vsync_maintainer.lua
+++ misc/reframework/autorun/exclusive_vsync_maintainer.lua
@@ -6,12 +6,14 @@ local show_custom_ticker = require("func/show_custom_ticker")
 local was_key_down = require("func/was_key_down")
 
 
-local x_start = 0
-local y_start = 0
+local x_start = 16
+local y_start = 16
+local width = 200
+local height = 75
 re.on_frame(function()
     if true then -- was_key_down(0x7B) then -- F12
-        draw.filled_rect(x_start, y_start, 200, 90, 0xC0000000)
-        draw.outline_rect(x_start, y_start, 200, 90, 0xFFFFFFFF)
+        draw.filled_rect(x_start, y_start, width, height, 0xC0000000)
+        draw.outline_rect(x_start, y_start, width, height, 0xFFFFFFFF)
         -- show_custom_ticker("VSync is")
     end
 end)
