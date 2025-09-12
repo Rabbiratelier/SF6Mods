c7f68667a1703cbc3c9123e3dfbc2cb7660dd51a
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-13 00:56:31 +0900

    Fix new_type_id function to correctly evaluate static value conditions for max_id calculation

--- experimental/reframework/autorun/optionmanip.lua
+++ experimental/reframework/autorun/optionmanip.lua
@@ -94,7 +94,7 @@ function my.new_type_id()
         return my.max_id
     end
     for _, i in ipairs(sdk.find_type_definition("app.Option.ValueType"):get_fields()) do
-        local value = i:is_static() and not i:get_data(nil) > 2100000000 and i:get_data(nil) or 0
+        local value = i:is_static() and i:get_data(nil) < 2100000000 and i:get_data(nil) or 0
         if value > my.max_id then
             my.max_id = value
         end
