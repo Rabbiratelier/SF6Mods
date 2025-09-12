8e51c2ab9248cdba90a1c3b66240b9033fd89238
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-09-13 01:16:16 +0900

    Initialize height and add dynamic height adjustment for VSync display in exclusive_vsync_maintainer.lua

--- misc/reframework/autorun/exclusive_vsync_maintainer.lua
+++ misc/reframework/autorun/exclusive_vsync_maintainer.lua
@@ -9,9 +9,14 @@ local was_key_down = require("func/was_key_down")
 local x_start = 16
 local y_start = 16
 local width = 200
-local height = 75
+local height = 0
+local height_increment = 3
+local height_max = 81
 re.on_frame(function()
     if true then -- was_key_down(0x7B) then -- F12
+        if height < height_max then
+            height = height + height_increment
+        end
         draw.filled_rect(x_start, y_start, width, height, 0xC0000000)
         draw.outline_rect(x_start, y_start, width, height, 0xFFFFFFFF)
         -- show_custom_ticker("VSync is")
